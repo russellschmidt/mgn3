21d0d9717a4f33c9c186e5fcb84b1519974a6993
Russell Schmidt <russell.schmidt@me.com>
2021-01-29 00:26:49 -0800

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,45 +3,19 @@ import logo from './logo.svg';
 import './App.scss';
 
 import { Auth, Hub } from 'aws-amplify';
+import Header from './Header'
+import Router from './Router'
 import styled from  'styled-components';
 
-function checkUser() {
-  Auth.currentAuthenticatedUser()
-    .then(user => console.log({ user }))
-    .catch(err => console.log(err))
-}
-
-function signOut() {
-  Auth.signOut()
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
-}
-
-function App(props) {
-  useEffect(() => {
-    Hub.listen('auth', (data) => {
-      const { payload } = data
-      console.log('A new auth event has occurred', data)
-      if (payload.event === 'signIn') {
-        console.log('a user has signed in!')
-      }
-      if (payload.event === 'signOut') {
-        console.log('a user has signed out!')
-      }
-    })
-  }, [])
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Circle Up</h1>
-        <Button onClick={() => Auth.federatedSignIn({provider: "Google"})}>Sign In with Google</Button>
-        <Button onClick={() => Auth.federatedSignIn({provider: "Facebook"})}>Sign In with Facebook</Button>
-        <Button onClick={checkUser}>Check User</Button>
-        <Button onClick={signOut}>Sign Out</Button>
-      </header>
-    </div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Router />
+      </div>
+    )
+  }
 }
 
 export default App
